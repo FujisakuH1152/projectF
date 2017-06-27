3fbf3b31ef1e79cfd330ec3e4b50fa783c484e84
toda.shinnosuke <toda.shinnosuke@toda>
2017-06-27 10:50:26 +0900

    Signed-off-by: toda.shinnosuke <toda.shinnosuke@toda>

--- projectf/src/jp/co/cominc/javalesson/projectf/controller/ProjectfFrontControllerServlet.java
+++ projectf/src/jp/co/cominc/javalesson/projectf/controller/ProjectfFrontControllerServlet.java
@@ -1,23 +1,34 @@
 package jp.co.cominc.javalesson.projectf.controller;
 
+
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
- * .Servlet implementation class ProjectfFrontControllerServlet
+ * Servlet implementation class ProjectfFrontControllerServlet
  */
 @WebServlet("*.do")
 public class ProjectfFrontControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	protected void doService(HttpServletRequest request, HttpServletResponse response){
-	
+	protected void doService(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		// リクエストURLから適切なActionオブジェクト(ビジネス・ロジックの実行をカプセル化するオブジェクト)を取得
+		Action action = ActionFactory.getAction(request.getServletPath());
+		// Actionを実行して、転送先Viewのパスを取得
+		String forwardPath = action.execute(request, response);
 		
+		// Actionの実装先でsendRedirectが実行されている場合にはレスポンスがすでに返送されているためnullが返る
+		if (forwardPath != null) { // レスポンスがまだ返送されていなければ
+			// 適切なViewに転送
+			request.getRequestDispatcher(forwardPath).forward(request, response);
+		}
 	}
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -36,3 +47,4 @@ public class ProjectfFrontControllerServlet extends HttpServlet {
 	}
 
 }
+
